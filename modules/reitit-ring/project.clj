3fb80bca1b40d649a5993f2f95fe9be499369cc4
Matthew Marks <matthewfmarks@gmail.com>
2024-07-07 18:09:21 -0400

    patch ring to support graal

--- modules/reitit-ring/project.clj
+++ modules/reitit-ring/project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/reitit-ring "0.7.1"
+(defproject metosin/reitit-ring "0.7.1-patched-graal"
   :description "Reitit: Ring routing"
   :url "https://github.com/metosin/reitit"
   :license {:name "Eclipse Public License"
@@ -10,4 +10,4 @@
   :parent-project {:path "../../project.clj"
                    :inherit [:deploy-repositories :managed-dependencies]}
   :dependencies [[metosin/reitit-core]
-                 [ring/ring-core]])
+                 [ring/ring-core "1.12.2-patched-graal"]])
